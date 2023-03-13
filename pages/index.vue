80a5bd6bb69dcadf2ef08cc226432ce52eb46b53
Paulina Shakirova <paulinashakirova@gmail.com>
2023-03-13 11:21:55 +0100

    feat: move router to the page level

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,48 @@
 <script setup lang="ts">
+import Pagination from '@/components/Pagination.vue'
 import ProductsList from '@/sections/ProductsList.vue'
+import {
+  computed,
+  onBeforeMount,
+  ref,
+  useRouter,
+} from '@nuxtjs/composition-api'
+
+import { useProductsListStore } from '@/stores/products'
+
+const store = useProductsListStore()
+const router = useRouter()
+
+const currentPage = ref<number>(1)
+
+const productsPerPage = computed(() => {
+  return Number(router.currentRoute.query.perPage)
+})
+
+const productsListLength = computed(() => {
+  return store.productsList.length
+})
+
+const productsCurrentPageFromQuery = computed(() => {
+  return Number(router.currentRoute.query.page)
+})
+
+const productsCurrentPage = computed(() => {
+  return store.productsList.slice(
+    currentPage.value - 1,
+    currentPage.value + Number(productsPerPage.value) - 1
+  )
+})
+
+const paginate = (page: number) => {
+  currentPage.value = page
+  router.push({ query: { page: page.toString(), perPage: '4' } })
+}
+
+onBeforeMount(async () => {
+  paginate(productsCurrentPageFromQuery.value)
+  await store.fetchProductsList()
+})
 </script>
 
 <template>
@@ -7,6 +50,11 @@ import ProductsList from '@/sections/ProductsList.vue'
     <h1 class="text-3xl md:text-4xl font-bold text-center py-2 my-8">
       Here is the list of all products
     </h1>
-    <ProductsList />
+    <ProductsList :products-current-page="productsCurrentPage" />
+    <Pagination
+      :total-products="productsListLength"
+      :products-per-page="productsPerPage"
+      @paginate="paginate"
+    />
   </div>
 </template>
--- sections/ProductsList.vue
+++ sections/ProductsList.vue
@@ -1,86 +1,42 @@
 <script setup lang="ts">
-import Pagination from '@/components/Pagination.vue'
 import ProductCard from '@/components/ProductCard.vue'
 import { roundPrice } from '@/lib/helpers/utils'
-import { useProductsListStore } from '@/stores/products'
-import {
-  computed,
-  onBeforeMount,
-  ref,
-  useRouter,
-} from '@nuxtjs/composition-api'
 
-const store = useProductsListStore()
-const router = useRouter()
-
-const currentPage = ref<number>(1)
-
-const productsPerPage = computed(() => {
-  return router.currentRoute.query.perPage
-})
-
-const productsList = computed(() => {
-  return store.productsList
-})
-
-const productsCurrentPageFromQuery = computed(() => {
-  return router.currentRoute.query.page
-})
-
-const productsCurrentPage = computed(() => {
-  return store.productsList.slice(
-    currentPage.value - 1,
-    currentPage.value + Number(productsPerPage.value) - 1
-  )
-})
-
-const paginate = (page: number) => {
-  currentPage.value = page
-  router.push({ query: { page: page.toString(), perPage: '4' } })
+interface Props {
+  productsCurrentPage: any
 }
 
-onBeforeMount(async () => {
-  await store.fetchProductsList()
-  paginate(Number(productsCurrentPageFromQuery.value))
-})
+const props = defineProps<Props>()
 </script>
 
 <template>
   <section>
-    <Pagination @paginate="paginate" :total-products="productsList.length">
-      <div class="flex flex-wrap gap-8 my-8 py-8">
-        <div
-          class="mx-auto"
-          v-for="{
-            id,
-            category, //what exactly do they mean?
-            description,
-            name,
-            discount: { status, value },
-            isActive,
-            picture,
-            price,
-            stock,
-          } of productsCurrentPage"
-          :key="id"
-        >
-          <ProductCard
-            :name="name"
-            :description="description"
-            :category="category"
-            :picture="picture"
-            :price="roundPrice(price)"
-            :stock="stock"
-            :discount="{ status, value }"
-          />
-        </div>
+    <div class="flex flex-wrap gap-8 my-8 py-8">
+      <div
+        class="mx-auto"
+        v-for="{
+          id,
+          category, //what exactly do they mean?
+          description,
+          name,
+          discount: { status, value },
+          isActive,
+          picture,
+          price,
+          stock,
+        } of props.productsCurrentPage"
+        :key="id"
+      >
+        <ProductCard
+          :name="name"
+          :description="description"
+          :category="category"
+          :picture="picture"
+          :price="roundPrice(price)"
+          :stock="stock"
+          :discount="{ status, value }"
+        />
       </div>
-    </Pagination>
+    </div>
   </section>
 </template>
-
-<!-- if on sale - pass sale prop... and display it somehow, but no logic inside 
-if in our out of stock... also pass and find representation
-and if isActive... 
-if(stock <= 0 => disable)
-HOW pass nested properties as props-->
