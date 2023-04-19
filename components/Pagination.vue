fb7730b7f9d38bf6ada865e9ced6d6e6c262f3af
Paulina Shakirova <paulinashakirova@gmail.com>
2023-04-19 13:58:02 +0200

    feat: improve pagination component

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { computed, useRoute } from '@nuxtjs/composition-api'
+import { computed, ref, useRoute } from '@nuxtjs/composition-api'
+
 const route = useRoute()
 
 interface Props {
@@ -10,28 +11,117 @@ interface Props {
 const props = defineProps<Props>()
 const emit = defineEmits(['paginate'])
 
+const currentPage = ref<number>(Number(route.value.query.page))
+
 const pages = computed(() => {
   return Math.ceil(
     props.totalProducts / (!props.productsPerPage ? 6 : props.productsPerPage)
   )
 })
 
+const pagesToDisplay = computed(() => {
+  let start = <number[]>[]
+  let middle = <number[]>[]
+  let end = <number[]>[]
+
+  if (pages.value <= 7) {
+    return {
+      start: Array.from({ length: pages.value }, (_, i) => i + 1),
+      middle: [],
+      end: [],
+    }
+  }
+
+  if (currentPage.value === 1) {
+    start = [1, 2]
+    middle = [~~(pages.value / 2)]
+    end = [pages.value - 1, pages.value]
+  } else if (currentPage.value === 2) {
+    start = [1, 2, 3]
+    middle = [~~(pages.value / 2)]
+    end = [pages.value - 1, pages.value]
+  } else if (currentPage.value === 3) {
+    start = [1, 2, 3, 4]
+    middle = []
+    end = [pages.value - 1, pages.value]
+  } else if (currentPage.value === 4) {
+    start = [1, 2, 3, 4, 5]
+    middle = []
+    end = [pages.value - 1, pages.value]
+  } else if (currentPage.value === pages.value) {
+    start = [1, 2]
+    middle = [~~(pages.value / 2)]
+    end = [pages.value - 1, pages.value]
+  } else if (currentPage.value === pages.value - 1) {
+    start = [1, 2]
+    middle = [~~(pages.value / 2)]
+    end = [pages.value - 2, pages.value - 1, pages.value]
+  } else if (currentPage.value === pages.value - 2) {
+    start = [1, 2]
+    middle = []
+    end = [pages.value - 3, pages.value - 2, pages.value - 1, pages.value]
+  } else if (currentPage.value === pages.value - 3) {
+    start = [1, 2]
+    middle = []
+    end = [
+      pages.value - 4,
+      pages.value - 3,
+      pages.value - 2,
+      pages.value - 1,
+      pages.value,
+    ]
+  } else {
+    start = [1, 2]
+    middle = [currentPage.value - 1, currentPage.value, currentPage.value + 1]
+    end = [pages.value - 1, pages.value]
+  }
+
+  return { start, middle, end }
+})
+
 const handleClick = (page: number) => {
+  currentPage.value = page
   emit('paginate', page)
 }
 </script>
 
 <template>
   <div>
-    <div class="flex gap-4 mx-auto my-4 flex-wrap justify-center">
-      <div v-for="page in pages" :key="page">
-        <button
-          @click="handleClick(page)"
-          class="py-2 px-4 border rounded-md"
-          :class="page === Number(route.query.page) ? 'bg-yellow-300' : ''"
-        >
-          {{ page }}
-        </button>
+    <div class="flex mx-auto my-4 justify-center border-2 border-blue-400">
+      <div class="flex flex-wrap gap-3 border-2 border-red-400">
+        <div v-for="page in pagesToDisplay.start" :key="page">
+          <button
+            @click="handleClick(page)"
+            class="py-2 px-4 border rounded-md"
+            :class="page === Number(route.query.page) ? 'bg-yellow-300' : ''"
+          >
+            {{ page }}
+          </button>
+        </div>
+        <template v-if="pagesToDisplay.middle.length">
+          <div>...</div>
+          <div v-for="page in pagesToDisplay.middle" :key="page">
+            <button
+              @click="handleClick(page)"
+              class="py-2 px-4 border rounded-md"
+              :class="page === Number(route.query.page) ? 'bg-yellow-300' : ''"
+            >
+              {{ page }}
+            </button>
+          </div>
+        </template>
+        <template v-if="pagesToDisplay.end.length">
+          <div>...</div>
+          <div v-for="page in pagesToDisplay.end" :key="page">
+            <button
+              @click="handleClick(page)"
+              class="py-2 px-4 border rounded-md"
+              :class="page === Number(route.query.page) ? 'bg-yellow-300' : ''"
+            >
+              {{ page }}
+            </button>
+          </div>
+        </template>
       </div>
     </div>
   </div>
